643a55be3026ff9cfb0cf957adeb4c97a43674a5
Diego Jacobo Martínez <diegojmr5@gmail.com>
2023-10-07 20:10:24 -0600

    Profile page

---
+++ src/components/Profile/ProfileUpdateModal.js
@@ -0,0 +1,148 @@
+import React from "react";
+import { Box, Typography, Button, Modal, Grid, FormControl, InputLabel, Input, FormHelperText, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
+import { getToken } from "../../models/token";
+
+const styleModalAdmin = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 0.5,
+    bgcolor: 'white',
+    border: '0.25rem solid #D9D9D9',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: '1rem',
+};
+
+export default function ProfileUpdateModal({ onSuccess }) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+
+    const isEmailValid = () => {
+        // Regular expression for email validation
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailRegex.test(email);
+    };
+
+    const displayHelperTextEmail = () => {
+        if (email && !isEmailValid()) {
+            return 'Invalid email address';
+        }
+        return '';
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        // Validate email and password
+        if (!isEmailValid()) {
+            return;
+        }
+
+        const requestData = {
+            email: email,
+            name: name
+        };
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${getToken()}`
+        };
+
+        try {
+            const response = await axios.patch(
+                `${process.env.REACT_APP_API_BASE_URL}/sel4c/user/me/`,
+                requestData,
+                { headers: headers }
+            );
+
+            // Handle success, show a success message and close the modal
+            console.log('Successfull request:');
+
+            // Clear the form
+            setName('');
+            setEmail('');
+            // Close the modal after a successful request
+            handleClose();
+            alert('Información actualizada');
+            // Pass the new user's data to the callback
+            if (typeof onSuccess === "function") {
+                onSuccess(response.data);
+            }
+        } catch (error) {
+            // Handle errors, delete on production?
+            console.error('Error:', error);
+            alert('Error al actualizar la información');
+        }
+    };
+
+    return (
+        <div>
+            <Button variant="contained" color="primary" onClick={handleOpen}>ACTUALIZAR INFORMACIÓN</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={styleModalAdmin}>
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        onClick={handleClose}
+                        aria-label="close"
+                        sx={{
+                            position: 'absolute',
+                            top: '0.3rem',
+                            right: '1rem',
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container direction='column' spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography variant="h5" align="center">Actualiza tu información</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <InputLabel htmlFor="name">Nombre</InputLabel>
+                                    <Input
+                                        id="name"
+                                        aria-describedby="name-helper-text"
+                                        type='text'
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <InputLabel htmlFor="email">Correo</InputLabel>
+                                    <Input
+                                        id="email"
+                                        aria-describedby="email-helper-text"
+                                        type='text'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                    <FormHelperText id="email-helper-text">{displayHelperTextEmail()}</FormHelperText>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <Button variant="contained" color="success" type="submit">Añadir</Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,34 @@
 import './Profile.sass'
 import React from "react";
 import Box from '@mui/material/Box';
+import ProfileUpdateModal from '../components/Profile/ProfileUpdateModal';
+import { getMe } from "../models/users";
 
 export default function Profile() {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const meData = await getMe();
+                setName(meData.name);
+                setEmail(meData.email);
+            } catch (error) {
+                // Handle errors, e.g., show an error message
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleUserDataUpdateSuccess = (userData) => {
+        setName(userData.name);
+        setEmail(userData.email);
+    }
+
+
     return (
         <div>
             <Box sx={{
@@ -16,10 +42,16 @@ export default function Profile() {
             }}>
                 <p className='title'>Esta es la página de profile</p>
                 <div className="info">
-                    <span className="label">Correo electrónico:</span> user@example.com
+                    <span className="label">Correo electrónico:</span> {email}
                 </div>
                 <div className="info">
-                    <span className="label">Nombre:</span> string
+                    <span className="label">Nombre:</span> {name}
+                </div>
+                <div>
+                    <ProfileUpdateModal onSuccess={handleUserDataUpdateSuccess} />
+                </div>
+                <div>
+                    Aqui deberia ir el link para recuperar contraseña
                 </div>
             </Box>
         </div>
